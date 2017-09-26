3ba0425dd6a4499f1b1cff8337e93dea4a456b92
S D Norman <crazynorm01@gmail.com>
2017-09-26 05:54:23 +0100

    Add project files.

---
+++ SDNorman/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SDNorman.MvcApplication" Language="C#" %>
---
+++ SDNorman/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SDNorman.Startup))]
+namespace SDNorman
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
