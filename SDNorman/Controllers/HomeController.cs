8f1f468470c2f6cd884536603d9d3aef2cd38e92
S D Norman <crazynorm01@gmail.com>
2017-09-26 08:43:20 +0100

    Commit test

--- SDNorman/Controllers/HomeController.cs
+++ SDNorman/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace SDNorman.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description";
 
             return View();
         }
